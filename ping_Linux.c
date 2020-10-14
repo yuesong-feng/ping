2a8392c1c5b6f46a9e25a155d1509ef5b73307cd
yuesong-feng <yuesong-feng@foxmail.com>
2020-10-14 11:38:57 +0100

    try Linux ping

--- ping_Linux.c
+++ ping_Linux.c
@@ -1,13 +1,71 @@
 #include <stdio.h>
-#include <sys/socket.h>
 
-int main(int argc, char *argv[])
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef struct
 {
-    int socket_desc;
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_desc == -1)
+    u8 version_and_headerlen;
+    u8 type_of_service;
+    u16 packet_len;
+    u16 id;
+    u16 slice_info;
+    u8 TTL;
+    u8 protocol;
+    u16 checksum;
+    u32 source_ip;
+    u32 dest_ip;
+} IP_HEADER;
+
+typedef struct
+{
+    u8 Type;
+    u8 Code;
+    u16 Checksum;
+    u16 Id;
+    u16 Seq;
+} ICMP_HEADER;
+
+int main(int argc, char const *argv[])
+{
+    ICMP_HEADER test;
+
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd == -1)
     {
-        printf("Could not create socket");
+        printf("create socket error!\n");
+        return -1;
     }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("61.135.169.191");
+    char buf[256];
+    ICMP_HEADER sendpack;
+    sendpack.Code=0;
+    
+    int send = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (send == -1)
+    {
+        printf("send error!\n");
+        return -1;
+    }
+    else
+    {
+        printf("you have sent %d bytes of data\n", send);
+    }
+
+    char recvpack[1024];
+    memset(recvpack, 0, sizeof(recvpack));
+    int len = sizeof(server_addr);
+    int recv = recvfrom(sockfd, recvpack, sizeof(recvpack), 0, (struct sockaddr *)&server_addr, &len);
+
     return 0;
 }
